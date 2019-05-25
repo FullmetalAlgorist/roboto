685f952a55029655ad07c53420b10a8000821e7f
akcrew <mariah.vicary@gmail.com>
2019-05-24 20:48:40 -0700

    card construction, props

---
+++ components/CardyB.vue
@@ -0,0 +1,51 @@
+<template>
+    <div >
+        <v-flex xs12 mb-2>
+            <v-card color="primary" class="white--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">{{ title }}</div>
+                      <div>{{ artistName }}</div>
+                      <div>({{ releaseDate }})</div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img
+                    :src="art"
+                    height="125px"
+                    contain
+                  >
+                  </v-img>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3">
+                Rate this album
+                <v-spacer></v-spacer>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+                <v-icon>star_border</v-icon>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+    </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+       
+    }
+  },
+   props: {title : String,
+            art: String,
+            artistName: String,
+            releaseDate: String
+    }
+}
+</script>
--- pages/results/_id.vue
+++ pages/results/_id.vue
@@ -1,16 +1,19 @@
 <template>
 <div class="results">
-    <div v-for="album in this.$store.state.albums" class="eachAlbum">
-        <h1 >{{ album.collectionName }}</h1>
-        <!-- <v-img :src="require(album.collectionViewUrl)"></v-img> -->
-    </div>
+    <cardy-b v-for="album in this.$store.state.albums"  :title="album.collectionName" :art="album.artworkUrl100" :artistName="album.artistName":releaseDate="album.releaseDate.substring(0,4)"/>
     </div>
 </template>
 <script>
+import CardyB from '~/components/CardyB.vue'
 export default {
     middleware: 'search',
+    components: {
+        'cardy-b': CardyB
+    },
     methods: {
-       
+    },
+    created(){
+         console.log(this.$store.state.albums[1]);
     }
 }
 </script>
